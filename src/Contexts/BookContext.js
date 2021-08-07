bc847ffd977326fe7892d655f345495b1f366bec
RaisanJMR <raisanjmr@protonmail.com>
2021-08-07 21:05:33 +0530

    working with reducers

--- src/Contexts/BookContext.js
+++ src/Contexts/BookContext.js
@@ -5,8 +5,8 @@ export const BookContext = createContext();
 
 const BookContextProvider = (props) => {
   const [books, setBooks] = useState([
-    { title: 'name of the wind', author: 'patrick rothfuss', id: 1 },
-    { title: 'the final empire', author: 'brandon sanderson', id: 2 },
+    { title: 'name of the wind', author: 'patrick rothfuss', id: uuid() },
+    { title: 'the final empire', author: 'brandon sanderson', id: uuid() },
   ]);
   console.log(books);
   const addBook = (title, author) => {
